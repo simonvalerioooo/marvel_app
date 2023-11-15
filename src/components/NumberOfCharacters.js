dc97b818be1b7ab4473ca042a1f5efe4e7deec32
Simon <simon.valerio@etu.univ-poitiers.fr>
2023-11-15 17:51:24 +0100

    Add unit tests for NumberOfCharacters

--- src/components/NumberOfCharacters.js
+++ src/components/NumberOfCharacters.js
@@ -1,7 +1,9 @@
 export function NumberOfCharacters({ characters = [] }) {
-    if (characters.length === 0) {
-      return <p>There is no characters</p>;
-    }
-
+  if (characters.length === 0) {
+    return <p>There is no characters</p>;
+  } else if (characters.length === 1) {
+    return <p>There is 1 character</p>;
+  } else {
     return <p>There is {characters.length} characters</p>;
-}  
+  }
+}
